c037dc21a7ea90f682871888666bd695c4d85e3c
ganeshraja10 <31185730+ganeshraja10@users.noreply.github.com>
2022-10-28 21:34:50 +0530

    -> Added Reminder script for leet code

---
+++ reminder/leet_code_daily_problem.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import requests
+import json
+import os
+
+LETT_CODE_URL = "https://leetcode.com/graphql/"
+
+LEET_CODE_USER_NAME='ganeshrajadev'
+
+json_array={
+  "query": "\n    query recentAcSubmissions($username: String!, $limit: Int!) {\n  recentAcSubmissionList(username: $username, limit: $limit) {\n    id\n    title\n    titleSlug\n    timestamp\n  }\n}\n    ",
+  "variables": {
+    "username": LEET_CODE_USER_NAME,
+    "limit": 10
+  }
+}
+
+r = requests.post(LETT_CODE_URL, json=json_array)
+
+submissions = json.loads(r.text)
+
+isSolvedToday=False
+
+for submission in  submissions['data']['recentAcSubmissionList']:
+  if "timestamp" in submission:
+    dt_object = datetime.fromtimestamp(int(submission["timestamp"]))
+    if dt_object.date() == datetime.today().date():
+      isSolvedToday=True
+      break
+  
+if not isSolvedToday:
+  os.system('notify-send "Solve Leet Code Problem" "You have yet to solve any leet code problem Today"')  
